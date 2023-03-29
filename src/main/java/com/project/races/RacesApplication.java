ed5a7fc3138e02e9220fcac62ff6c3e22f583f30
ospodaryk <ospodaryk11@gmail.com>
2023-03-29 16:26:54 +0300

    Init

---
+++ src/main/java/com/project/races/RacesApplication.java
@@ -0,0 +1,13 @@
+package com.project.races;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/races/RacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.races;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
