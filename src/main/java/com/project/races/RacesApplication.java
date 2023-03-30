233e0c1045571e61a0a34e23065a88a310178252
ospodaryk <ospodaryk11@gmail.com>
2023-03-30 11:24:41 +0300

    Refactored code

--- src/main/java/com/project/races/RacesApplication.java
+++ src/main/java/com/project/races/RacesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RacesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RacesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RacesApplication.class, args);
+    }
 
 }
