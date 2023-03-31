c7c6b186e82b3a22be4d39f7f577de0a08243cea
ospodaryk <ospodaryk11@gmail.com>
2023-03-31 12:43:58 +0300

    Added schedule to exact data which sent with object

--- src/main/java/com/project/races/timer/Scheduler.java
+++ src/main/java/com/project/races/timer/Scheduler.java
@@ -3,9 +3,12 @@ package com.project.races.timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @Component
 public class Scheduler {
     private static final Logger logger = LoggerFactory.getLogger(Scheduler.class);
@@ -13,10 +16,28 @@ public class Scheduler {
     @Autowired
     private KafkaProducer kafkaProducer;
 
-    @Scheduled(fixedDelay = 10000)
+    @Autowired
+    private SchedulerConfig schedulerConfig;
+
+    @Scheduled(cron = "#{@schedulerConfig.getCronExpression()}")
     public void scheduleTask() {
-        logger.info("________________Scheduler");
-        System.out.println("________________Scheduler");
-        kafkaProducer.sendMessage(new MyMessage("1","jejej"));
+        kafkaProducer.sendMessage(new MyMessage("1", "jejej"));
+    }
+}
+
+@Component
+class SchedulerConfig {
+    private final String cronExpression;
+
+    public SchedulerConfig() {
+        LocalDateTime date = LocalDateTime.of(2023, 03, 31, 12, 43, 0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ss mm HH dd MM *");
+        System.out.println(date.format(formatter));
+        this.cronExpression = date.format(formatter);
+    }
+
+    public String getCronExpression() {
+//        return "00 41 12 31 03 *";
+        return this.cronExpression;
     }
 }
