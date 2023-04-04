d7f759528eb3d72821571bc7a882cef859ae4ee5
ospodaryk <ospodaryk11@gmail.com>
2023-04-04 21:33:59 +0300

    Optimised imports

--- src/main/java/com/project/races/controller/RaceController.java
+++ src/main/java/com/project/races/controller/RaceController.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/project/races/controller/TeamController.java
+++ src/main/java/com/project/races/controller/TeamController.java
@@ -8,13 +8,11 @@ import com.project.races.model.Team;
 import com.project.races.service.RaceService;
 import com.project.races.service.TeamService;
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
